4aba9a67b07ed2ab3481dc129587f21ce30a3ff8
dsmd <lex.mironchenko@gmail.com>
2022-09-23 21:12:41 +0100

    sample GET with Request body

--- src/main/java/me/dsmd/GreetingResource.java
+++ src/main/java/me/dsmd/GreetingResource.java
@@ -1,9 +1,14 @@
 package me.dsmd;
 
+import me.dsmd.cat8rat.dto.SampleDTO;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class GreetingResource {
@@ -12,5 +17,13 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Cat8Rat HWS";
+    }
+
+    @GET
+    @Path("/sample")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response sample(SampleDTO sampleDTO) {
+        return Response.ok(sampleDTO).build();
     }
 }
